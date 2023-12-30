53011e81f31fea1a87564b6732f2a4fcbc6e45cf
Paul Drake <pdrake1@me.com>
2023-12-30 15:29:22 -0600

    Initial commit

---
+++ src/main/kotlin/com/pdrake/musicdirectclone/MusicDirectCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.pdrake.musicdirectclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicDirectCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusicDirectCloneApplication>(*args)
+}
---
+++ src/test/kotlin/com/pdrake/musicdirectclone/MusicDirectCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pdrake.musicdirectclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicDirectCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
